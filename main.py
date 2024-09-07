6fdf8743f5821882a206fd5cba40e393aa475956
dbink-t495 <dbinkv1@Gmail.com>
2024-09-07 11:31:13 +0800

    添加按键中断功能以初始化时间
    在现有RTC时间显示功能的基础上，新增了按键中断处理，用于在用户按下按键时初始化时间。实现了一个定时器中断回调，用于处理按键去抖动，并在按下按键时增加初始化时间。同时，代码还优化了显示时间和日期格式的逻辑，使其符合常见的显示顺序。
    
    本次更新主要实现了通过硬件按键调整内部RTC时间的功能，提升了用户与系统交互的便捷性。

--- main.py
+++ main.py
@@ -7,29 +7,49 @@ rtc = RTC()
 i2c = SoftI2C(scl=Pin(1), sda=Pin(0))
 oled = SSD1306_I2C(width=128, height=64, i2c=i2c)
 
-# 清屏
-oled.fill(0)
+KEY = Pin(9, Pin.IN, Pin.PULL_UP)  # 构建KEY对象
 
+init_s = 0
+cnt_s  = 0
+
+def init_time(KEY_callback):
+    global init_s, cnt_s
+    time.sleep_ms(10)  # 消除抖动
+    if KEY.value() == 0:  # 确认按键被按下
+        init_s += 5  # 增加5秒
+        cnt_s   = time.time()
 # 定义定时器中断的回调函数
 def main(timer_callback):
+    global init_s, cnt_s
     
-    oled.fill(0) # 清屏
+    oled.fill(0)  # 清屏
     
     date_time = rtc.datetime()
     
-    # 设置时间和日期的格式s
-    time_str = f'{date_time[1]:02}.{date_time[2]:02}'  # mm:dd
-    date_str = f'{date_time[4]:02}:{date_time[5]:02}:{date_time[6]:02}' # hh:mm:ss
+    # 设置时间和日期的格式
+    date_str = f'{date_time[1]:02}.{date_time[2]:02}'  # mm:dd
+    time_str = f'{date_time[4]:02}:{date_time[5]:02}:{date_time[6]:02}'  # hh:mm:ss
 
     print(time_str)
     
-    oled.text(time_str, 10, 10)
-    oled.text(date_str, 10, 25)
+    oled.text(date_str, 10, 10)
+    oled.text(time_str, 10, 25)
 
     oled.block(1, 1, 128, 64)  # UI 矩形屏幕边框
+
+    cnt = (init_s + cnt_s) - time.time()
+
+    if cnt < 0:
+        oled.text(f'times up {cnt} s', 10, 40)
+        print("时间到")
+    elif cnt > 0:
+        oled.text(f'cnt down {cnt} s', 10, 40)
+    
+    print(f"倒计时 {cnt} s")
     
     oled.show()
 
-# 定义定时器
-timer = Timer(0)
+KEY.irq(init_time, Pin.IRQ_FALLING)  # 定义中断，下降沿触发
+
+timer = Timer(0)  # 定义定时器
 timer.init(period=1000, mode=Timer.PERIODIC, callback=main)
